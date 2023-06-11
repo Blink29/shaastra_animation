a9e5ac81ce7dca7b0aa41dfb1bffef5ca01f560b
dusklight00 <r.rahul.developer@gmail.com>
2023-06-11 11:56:27 +0530

    got animation to work

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
 const textElement = document.querySelector(".text");
 const text = textElement.textContent;
 
-// setTimeout(() => {
-//   // textElement.textContent = '';
-//   for (let i = 0; i < text.length; i++) {
-//     const letter = text[i];
-//     const span = document.createElement("span");
-//     span.classList.add("letter");
-//     span.textContent = letter;
-//     span.style.animation = `slideLetter 2s ease-in-out ${i * 100}ms forwards`;
-//     textElement.appendChild(span);
-//   }
-// }, 5000);
-
-// setTimeout(() => {
-//   textElement.textContent = "";
-//   for (let i = 0; i < text.length; i++) {
-//     const letter = text[i];
-//     const span = document.createElement("span");
-//     span.classList.add("letter-disappear");
-//     span.textContent = letter;
-//     span.style.animationDelay = `${i * 100}ms`;
-//     textElement.appendChild(span);
-//     // textElement.textContent = ''
-//     // textElement.style.display = "flex";
-//     // textElement.style.textAlign = "center";
-//   }
-// }, 5000);
-
-// setTimeout(() => {
-//     textElement.textContent = ''
-// }, 5000)
-
 let letterSpans = [];
+let overlapLetterSpans = [];
 
 function convertToSpan() {
   textElement.textContent = "";
@@ -43,6 +13,16 @@ function convertToSpan() {
     textElement.appendChild(span);
     letterSpans.push(span);
   }
+  const overlapContainer = document.createElement("div");
+  overlapContainer.style = "position: absolute; top: 0px;";
+  for (index in text) {
+    const letter = text[index];
+    const span = document.createElement("span");
+    span.textContent = letter;
+    overlapContainer.appendChild(span);
+    overlapLetterSpans.push(span);
+  }
+  textElement.appendChild(overlapContainer);
 }
 
 function animateText() {
@@ -51,7 +31,14 @@ function animateText() {
     span.classList.add("letter-disappear");
     span.style.animationDelay = `${index * 100}ms`;
   }
+  for (index in overlapLetterSpans) {
+    overlapSpan = overlapLetterSpans[index];
+    overlapSpan.classList.add("letter");
+    overlapSpan.style.animation = `slideLetter 2s ease-in-out ${
+      index * 100
+    }ms forwards`;
+  }
 }
 
 convertToSpan();
-animateText();
+setTimeout(animateText, 5000);
