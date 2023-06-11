de3d2752452123404dccb0ef78f60332d2f57c39
dusklight00 <r.rahul.developer@gmail.com>
2023-06-11 13:04:04 +0530

    animation completed #1

--- app.js
+++ app.js
@@ -45,6 +45,30 @@ function animateLogo() {
   logo.style.animation = "logoAnimation 3s forwards";
 }
 
+function showBackground() {
+  const backgroundElem = document.querySelector(".backgroundElem");
+  backgroundElem.style.opacity = "1";
+}
+
+function bounceDice() {
+  const dice = document.querySelector(".dice");
+  dice.style.animation = "bounce 5s ease-in-out";
+}
+
+function dropDice() {
+  const dice = document.querySelector(".dice");
+  dice.style.animation = "diceDrop 1s ease-in-out";
+}
+
+function hideDice() {
+  const dice = document.querySelector(".dice");
+  dice.style.display = "none";
+}
+
 convertToSpan();
 setTimeout(animateText, 5000);
 setTimeout(animateLogo, 10000);
+setTimeout(showBackground, 10000);
+setTimeout(dropDice, 4000);
+setTimeout(bounceDice, 5000);
+setTimeout(hideDice, 10000);
